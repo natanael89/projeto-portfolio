352e22af8cd212315d363b70fb15ddb47452e07c
natanael carvalho dos santos <natanaelc7@gmail.com>
2025-06-02 23:28:00 -0300

    configurando projeto

--- src/components/layout/Cursos.jsx
+++ src/components/layout/Cursos.jsx
@@ -10,12 +10,22 @@ export const Cursos = () => {
                 <div className="grid md:grid-cols-2 gap-4">
                     <CardCursos
                         title="React"
-                        description="Projeto desenvolvido para o curso de React do DevFlix"
+                        description="Curso de React na escola Infinity School"
                         img="https://user-images.githubusercontent.com/97989643/220242520-78dd8232-4416-461a-a8f1-6c0b3f5f357f.gif"
                     />
                     <CardCursos
+                        title="GitHub"
+                        description="Curso de GitHub na escola OneBitCode"
+                        img="https://cdn.hashnode.com/res/hashnode/image/upload/v1658567611905/kMCMR25Vh.gif"
+                    />
+                    <CardCursos
+                        title="HTML,CSS,JS"
+                        description="Curso de HTML,CSS,JS na escola Infinity School"
+                        img="https://miro.medium.com/v2/resize:fit:1400/1*Jek77W1UJM-DZgpcG9nFsw.png"
+                    />
+                    <CardCursos
                         title="NodeJS"
-                        description="Projeto desenvolvido para o curso de React do DevFlix"
+                        description="Curso de NodeJS na escola OneBitCode"
                         img="https://miro.medium.com/v2/resize:fit:1000/1*LGNLwf-3dknhu1mMalKDJQ.gif"
                     />
                 </div>
--- src/components/utils/CardCursos.jsx
+++ src/components/utils/CardCursos.jsx
@@ -2,9 +2,19 @@
 export const CardCursos = ({title, description, img}) => {
     return (
         <div className="bg-dark border border-neon p-4 rounded-lg shadow-lg">
-            <img src={img} alt="" className="w-48 object-cover"/>
-            <h3 className="text-lg text-white font-pixel">{title}</h3>
-            <p className="text-sm text-gray-400">{description}</p>
+            <div className="flex flex-col items-center">
+                <div className="flex justify-center">
+                <img src={img} alt="" className="w-64 object-cover"/>
+                </div>
+                <div className="flex flex-col">
+                    <div className="flex justify-center">
+                     <h3 className="text-lg text-white font-pixel">{title}</h3>
+                    </div>
+                    <div className="flex justify-end">
+                     <p className="text-sm text-gray-400">{description}</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
